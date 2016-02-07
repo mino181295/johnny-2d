6ca1482dbcf72b46939f5cdadbeda087ff2edeb6
Matteo Minardi <7540687+mino181295@users.noreply.github.com>
2016-02-07 19:15:28 +0100

    modified launcher

--- oop15-johnny2d/src/it/unibo/oop/view/Launcher.java
+++ oop15-johnny2d/src/it/unibo/oop/view/Launcher.java
@@ -3,7 +3,9 @@ package it.unibo.oop.view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -14,36 +16,35 @@ import javax.swing.JPanel;
 public class Launcher extends JFrame {
 
 	private static final long serialVersionUID = -6224390548062243879L;
-	private static final Color RED = new Color(255, 100, 100);
-	private static final Color GREEN = new Color(100, 255, 100);
-	private static final Color BLUE = new Color(100, 100, 255);
+	private static final Color BUTTONS_COLOR = new Color(255, 220, 130);
 	private static final int N_BUTTONS = 3;
-	private static final int SCREEN_PROPORTION = 2;
-	private static final int BUTTONS_PROPORTION = 6;
 	
 	private Launcher() {
 		super("Johnny2D Launcher");
-		final Dimension dim = this.getToolkit().getScreenSize();
-		this.setSize(dim.width/SCREEN_PROPORTION, dim.height/SCREEN_PROPORTION);
+		final Dimension prefButtonSize = new Dimension(200, 50);
+		this.setSize(500, 500);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationByPlatform(true);
 		this.getContentPane().setLayout(new BorderLayout());
 		
 		JLabel background = new JLabel(new ImageIcon(Launcher.class.getResource("/background.jpg")));
-		background.setLayout(new BorderLayout());
+		background.setLayout(new FlowLayout());
 		JPanel buttonsPanel = new JPanel(new GridLayout(N_BUTTONS, 1));
-		buttonsPanel.setSize(dim.width/BUTTONS_PROPORTION, dim.height/BUTTONS_PROPORTION);
+		
 		final JButton play = new JButton("Play");
-		play.setBackground(RED);
+		play.setPreferredSize(prefButtonSize);
+		play.setBackground(BUTTONS_COLOR);
 		final JButton options = new JButton("Options");
-		options.setBackground(GREEN);
+		play.setPreferredSize(prefButtonSize);
+		options.setBackground(BUTTONS_COLOR);
 		final JButton quit = new JButton("Quit");
-		quit.setBackground(BLUE);
+		play.setPreferredSize(prefButtonSize);
+		quit.setBackground(BUTTONS_COLOR);
 		
 		buttonsPanel.add(play);
 		buttonsPanel.add(options);
 		buttonsPanel.add(quit);
-		background.add(buttonsPanel, BorderLayout.CENTER);
+		background.add(buttonsPanel);
 		this.getContentPane().add(background);
 		this.setVisible(true);
 	}
