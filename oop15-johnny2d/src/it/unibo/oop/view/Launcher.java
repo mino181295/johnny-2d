2e1d48c7aaf6d0827f5e03477597564d905976b6
Paolo Venturi <paolo.venturi9@studio.unibo.it>
2016-02-08 16:07:07 +0100

    class ImagePanel created to help the game menu. game menu improved.

--- oop15-johnny2d/src/it/unibo/oop/view/Launcher.java
+++ oop15-johnny2d/src/it/unibo/oop/view/Launcher.java
@@ -1,52 +1,69 @@
 package it.unibo.oop.view;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Launcher extends JFrame {
 
 	private static final long serialVersionUID = -6224390548062243879L;
 	private static final Color BUTTONS_COLOR = new Color(255, 220, 130);
-	private static final int N_BUTTONS = 3;
 	
 	private Launcher() {
 	    
 		super("Johnny2D Launcher");
+		
 		final Dimension prefButtonSize = new Dimension(200, 50);
 		this.setSize(500, 500);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocationByPlatform(true);
-		this.getContentPane().setLayout(new BorderLayout());
+		// this.setLocationByPlatform(true);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+						
+		final URL imgURL = Launcher.class.getResource("/background.jpg");
+		final ImageIcon img = new ImageIcon(imgURL);
 		
-		JLabel background = new JLabel(new ImageIcon(Launcher.class.getResource("/background.jpg")));
-		background.setLayout(new FlowLayout());
-		JPanel buttonsPanel = new JPanel(new GridLayout(N_BUTTONS, 1));
+		final JPanel menuPanel = new ImagePanel(img.getImage()); 
+		menuPanel.setLayout(new GridBagLayout());
 		
+		/*
+		 * BUTTONS CREATION
+		 */
+		
+		/* PLAY */
 		final JButton play = new JButton("Play");
 		play.setPreferredSize(prefButtonSize);
 		play.setBackground(BUTTONS_COLOR);
+		
+		/* OPTIONS */
 		final JButton options = new JButton("Options");
-		play.setPreferredSize(prefButtonSize);
+		options.setPreferredSize(prefButtonSize);
 		options.setBackground(BUTTONS_COLOR);
-		final JButton quit = new JButton("Quit");
-		play.setPreferredSize(prefButtonSize);
+		
+		/* QUIT */
+	    final JButton quit = new JButton("Quit");
+		quit.setPreferredSize(prefButtonSize);
 		quit.setBackground(BUTTONS_COLOR);
 		
-		buttonsPanel.add(play);
-		buttonsPanel.add(options);
-		buttonsPanel.add(quit);
-		background.add(buttonsPanel);
-		this.getContentPane().add(background);
+		/* ADD BUTTONS */
+		final GridBagConstraints cnst = new GridBagConstraints();
+		cnst.insets = new Insets(3, 3, 3, 3);
+		cnst.gridy = 0;
+		menuPanel.add(play, cnst);
+		cnst.gridy++;
+		menuPanel.add(options, cnst);
+		cnst.gridy++;
+		menuPanel.add(quit, cnst);
+	
+		this.setContentPane(menuPanel);
 		this.setVisible(true);
 	}
 	
