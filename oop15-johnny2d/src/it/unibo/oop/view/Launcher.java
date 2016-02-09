c89698845ffa554d87bdc5f273cb4714b2aa055e
Matteo Minardi <7540687+mino181295@users.noreply.github.com>
2016-02-09 02:41:07 +0100

    hgignore change

---
+++ oop15-johnny2d/src/it/unibo/oop/view/Launcher.java
@@ -0,0 +1,73 @@
+package it.unibo.oop.view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Launcher extends JFrame {
+
+	private static final long serialVersionUID = -6224390548062243879L;
+	private static final Color BUTTONS_COLOR = new Color(255, 220, 130);
+	
+	private Launcher() {
+	    
+		super("Johnny2D Launcher");
+		
+		final Dimension prefButtonSize = new Dimension(200, 50);
+		this.setSize(500, 500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// this.setLocationByPlatform(true);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+						
+		final URL imgURL = Launcher.class.getResource("/background.jpg");
+		final ImageIcon img = new ImageIcon(imgURL);
+		
+		final JPanel menuPanel = new ImagePanel(img.getImage()); 
+		menuPanel.setLayout(new GridBagLayout());
+		
+		/*
+		 * BUTTONS CREATION
+		 */
+		
+		/* PLAY */
+		final JButton play = new JButton("Play");
+		play.setPreferredSize(prefButtonSize);
+		play.setBackground(BUTTONS_COLOR);
+		
+		/* OPTIONS */
+		final JButton options = new JButton("Options");
+		options.setPreferredSize(prefButtonSize);
+		options.setBackground(BUTTONS_COLOR);
+		
+		/* QUIT */
+	    final JButton quit = new JButton("Quit");
+		quit.setPreferredSize(prefButtonSize);
+		quit.setBackground(BUTTONS_COLOR);
+		
+		/* ADD BUTTONS */
+		final GridBagConstraints cnst = new GridBagConstraints();
+		cnst.insets = new Insets(3, 3, 3, 3);
+		cnst.gridy = 0;
+		menuPanel.add(play, cnst);
+		cnst.gridy++;
+		menuPanel.add(options, cnst);
+		cnst.gridy++;
+		menuPanel.add(quit, cnst);
+	
+		this.setContentPane(menuPanel);
+		this.setVisible(true);
+	}
+	
+	public static void main(String... args) {
+		new Launcher();
+	}
+}
