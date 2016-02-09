4736f28586991c44761813afd9aed5b234a22a98
Matteo Minardi <7540687+mino181295@users.noreply.github.com>
2016-02-09 16:03:57 +0100

    Some interfaces added

---
+++ oop15-johnny2d/src/it/unibo/oop/model/Creature.java
@@ -0,0 +1,5 @@
+package it.unibo.oop.model;
+
+public interface Creature extends Entity {
+
+}
--- oop15-johnny2d/src/it/unibo/oop/utilities/Point2D.java
+++ oop15-johnny2d/src/it/unibo/oop/utilities/Point2D.java
@@ -1,5 +1,14 @@
 package it.unibo.oop.utilities;
 
+/**
+ * Interface that rappresents a single point in the screen
+ * @author Matteo Minardi
+ * 
+ */
 public interface Point2D {
+	
+	int getX();
+	
+	int getY();
 
 }
