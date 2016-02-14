33530d01b6cf700db1bfaeb5152cb7a7a35c4348
Matteo Minardi <7540687+mino181295@users.noreply.github.com>
2016-02-14 18:59:51 +0100

    Added an enum rapprresenting the characters settings(Vel, dimensions etc..)

---
+++ oop15-johnny2d/src/it/unibo/oop/utilities/CharactersSettings.java
@@ -0,0 +1,31 @@
+package it.unibo.oop.utilities;
+
+public enum CharactersSettings {
+	MAIN_CHARACTER(32,32,new Velocity(30,100, 10)),
+	BASIC_ENEMY(32,32,new Velocity(30,100, 10)),
+	BULLET(32,32,new Velocity(30,100, 10)),
+	BONUS(32,32,null);
+	
+	int width;
+	int height;
+	Velocity speed;
+	
+	private CharactersSettings(int width, int height, Velocity speed){
+		this.width = width;
+		this.height = height;
+		this.speed = speed;
+	}
+
+	public int getWidth() {
+		return this.width;
+	}
+
+	public int getHeight() {
+		return this.height;
+	}
+
+	public Velocity getSpeed() {
+		return this.speed;
+	}
+	
+}
