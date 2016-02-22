836574a9bbef57c9d3d7ae52a507ed1e7998ecc5
Paolo Venturi <paolo.venturi9@studio.unibo.it>
2016-02-22 15:37:01 +0100

    KeyCommands modified.

---
+++ oop15-johnny2d/src/it/unibo/oop/model/Factory.java
@@ -0,0 +1,82 @@
+package it.unibo.oop.model;
+
+import static it.unibo.oop.utilities.CharactersSettings.BASIC_ENEMY;
+import static it.unibo.oop.utilities.CharactersSettings.MAIN_CHARACTER;
+import static it.unibo.oop.utilities.CharactersSettings.WALL;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unibo.oop.utilities.Position;
+import it.unibo.oop.utilities.Vector2;
+
+public class Factory {
+	
+	public static class WallFactory {
+		
+		private WallFactory() {
+		}
+				
+		public static List<Wall> generateHorizontalWall(final int startingX, final int startingY, final int dimension){
+			final List<Wall> resultList = new ArrayList<>(dimension);
+			for(int offset = 0; offset < WALL.getWidth()*dimension; offset++ ){
+				resultList.add(new Wall(offset + WALL.getWidth()/2, startingY));
+			}			
+			return resultList;
+		}
+		
+		public static List<Wall> generateVerticalWall(final int startingX, final int startingY, final int dimension){
+			final List<Wall> resultList = new ArrayList<>(dimension);
+			for(int offset = 0; offset < WALL.getHeight()*dimension; offset++ ){
+				resultList.add(new Wall(startingX, offset + WALL.getHeight()/2));
+			}			
+			return resultList;
+		}
+		
+		public static Arena generateArena(final int panelHeight, final int panelWidth){
+			//TODO settare l'hud dimension
+			return new Arena(panelHeight,panelWidth,(int)(panelHeight*0.2));
+		}
+	}
+	
+	public static class MainCharacterFactory {
+		
+		private MainCharacterFactory() {
+		}
+	
+		public static MainCharacter generateCentredCharacter(final Position centerPos){
+			return new MainCharacter(centerPos.getX(), centerPos.getY());
+		}
+		public static MainCharacter generateStillCharacter(final double startingX, final double startingY){
+			return new MainCharacter(startingX,startingY);
+		}
+		
+		public static MainCharacter generateMovingCharacter(final double startingX, final double startingY, final Vector2 movement){
+			return new MainCharacter(startingX,startingY,movement,MAIN_CHARACTER.getSpeed());
+		}
+	}
+	
+	public static class EnemiesFactory {
+		
+		private EnemiesFactory() {
+		}
+		
+		public static BasicMonster generateStillBasicEnemy(final double intialX, final double intialY){
+			return new BasicMonster(intialX, intialY, new Vector2(), BASIC_ENEMY.getSpeed());
+		}
+	}
+	
+	public static class BulletFactory {
+		
+		private BulletFactory() {
+		}
+		
+		public static Bullet shootBulletFromCharacter(final MainCharacter mainChar){
+			return new Bullet(mainChar);
+		}
+		public static Bullet createBullet(final double startingX, final double startingY, final Vector2 movementVector){
+			return new Bullet(startingX, startingY, movementVector);
+		}
+	}
+
+}
---
+++ oop15-johnny2d/src/it/unibo/oop/view/MenuPanel.java
@@ -0,0 +1,106 @@
+package it.unibo.oop.view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+import it.unibo.oop.controller.AppState;
+import it.unibo.oop.controller.StateObserver;
+
+/**
+ * Custom {@link javax.swing.JPanel} used as template for game's menu. It defines standard buttons,
+ * layout and background image.
+ */
+public class MenuPanel extends BackgroundPanel implements MenuInterface {
+
+    private static final long serialVersionUID = 1L;
+    private static final Color COMPONENTS_COLOR = new Color(255, 220, 130);
+    private static final int TOP_INSET = 5;
+    private static final int LEFT_INSET = 5;
+    private static final int BOTTOM_INSET = 5;
+    private static final int RIGHT_INSET = 5;
+    private final Dimension prefComponentSize = new Dimension(160, 40);
+    private final GridBagConstraints cnst = new GridBagConstraints();
+    private final List<StateObserver> obsList;
+
+    /**
+     * Creates a new panel.
+     */
+    public MenuPanel() {
+        super("/background.jpg");
+
+        this.cnst.insets = new Insets(TOP_INSET, LEFT_INSET, BOTTOM_INSET, RIGHT_INSET);
+        this.cnst.gridy = 0;
+        this.setLayout(new GridBagLayout());
+        this.obsList = new ArrayList<>();
+    }
+
+    @Override
+    public void addComponent(final JComponent cmp, final boolean customize) {
+        if (customize) {
+            cmp.setPreferredSize(this.prefComponentSize);
+            cmp.setBackground(COMPONENTS_COLOR);
+        }
+        this.add(cmp, cnst);
+        cnst.gridy++;
+
+    }
+
+    @Override
+    public void addObserver(final StateObserver obs) {
+        this.obsList.add(obs);
+    }
+
+    @Override
+    public void addStateButton(final StateButton... btns) {
+        Arrays.asList(btns).forEach(btn -> {
+            final JButton jBtn = new JButton(btn.getName());
+            jBtn.addActionListener((e) -> this.doObsAction(obs -> new Thread(() -> 
+                                        obs.stateAction(btn.getState())).start()));
+            this.addComponent(jBtn, true);
+        });
+    }
+
+    @Override
+    public void setIcon(final String path) {
+        final URL imgURL = MenuPanel.class.getResource(path);
+        final ImageIcon icon = new ImageIcon(imgURL);
+        final JLabel label = new JLabel(icon);
+        this.addComponent(label, false);
+    }
+
+    @Override
+    public void doObsAction(final Consumer<StateObserver> action) {
+        this.obsList.forEach(action);
+    }
+    
+    public static class StateButton {
+        private final String name;
+        private final AppState state;
+
+        public StateButton(final String name, final AppState state) {
+            this.name = name;
+            this.state = state;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public AppState getState() {
+            return this.state;
+        }
+    }
+}
