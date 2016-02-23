3864a184ad3f4c5ce403b940caecd4a9e1737a45
Matteo Minardi <7540687+mino181295@users.noreply.github.com>
2016-02-23 17:36:29 +0100

    Added a temp coin spawn

---
+++ oop15-johnny2d/src/it/unibo/oop/exceptions/CollisionHandlingException.java
@@ -0,0 +1,14 @@
+package it.unibo.oop.exceptions;
+
+public class CollisionHandlingException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public CollisionHandlingException() {
+		super();
+	}
+	public CollisionHandlingException (String message) {
+		  super(message); 
+	}
+
+}
