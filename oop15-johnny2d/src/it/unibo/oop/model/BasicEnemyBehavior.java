33a0281e0e106583881d6a12ee6748f9c23248b2
Paolo Venturi <paolo.venturi9@studio.unibo.it>
2016-02-24 16:12:42 +0100

    some improvements.

---
+++ oop15-johnny2d/src/it/unibo/oop/model/BasicEnemyBehavior.java
@@ -0,0 +1,24 @@
+package it.unibo.oop.model;
+
+import it.unibo.oop.utilities.Position;
+import it.unibo.oop.utilities.Vector2;
+
+/**
+ * {@link Enemy} behavior that follows the {@link MainCharacter}.
+ */
+public class BasicEnemyBehavior implements MovementBehavior{
+	
+	private final MovableEntity playerPosition;
+	
+	public BasicEnemyBehavior(final BasicMonster player){
+		this.playerPosition = player;
+	}
+	
+	/**
+	 * Returns a vector that indicates where the {@link Enemy} should go to follow the {@link MainCharacter}.
+	 */
+	public Vector2 getNextMove(final Position targetPosition) {
+		final Vector2 newMovement = new Vector2(targetPosition.getX() - playerPosition.getX(), targetPosition.getY() - playerPosition.getY());
+		return newMovement.setLength(playerPosition.getVelocity().getMaxVelocity());
+	}
+}
