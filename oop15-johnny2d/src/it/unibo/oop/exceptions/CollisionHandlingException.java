73610cac30136c280d4cab30f55a74ceeb98bf9d
Giacomo Pasini <giacomo.pasini5@studio.unibo.it>
2016-02-25 15:14:30 +0100

    added randomly loaded arenas

--- oop15-johnny2d/src/it/unibo/oop/exceptions/CollisionHandlingException.java
+++ oop15-johnny2d/src/it/unibo/oop/exceptions/CollisionHandlingException.java
@@ -1,5 +1,8 @@
 package it.unibo.oop.exceptions;
 
+/**
+ * An exception that can occur in a collision.
+ */
 public class CollisionHandlingException extends Exception {
 
     private static final long serialVersionUID = -3161439127315782693L;
--- oop15-johnny2d/src/it/unibo/oop/exceptions/SpritesNotSplittableException.java
+++ oop15-johnny2d/src/it/unibo/oop/exceptions/SpritesNotSplittableException.java
@@ -1,5 +1,9 @@
 package it.unibo.oop.exceptions;
 
+/**
+ * An exception that can occur when splitting sprites from a
+ * {@link it.unibo.oop.view.SpriteSheet}.
+ */
 public class SpritesNotSplittableException extends Exception {
 
     private static final long serialVersionUID = -5160337647169133194L;
