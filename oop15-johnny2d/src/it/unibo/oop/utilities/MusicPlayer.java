84e701e1622df382100550a6e9ac49d4ea4e9fb5
Giacomo Pasini <giacomo.pasini5@studio.unibo.it>
2016-02-27 17:21:32 +0100

    MusicPlayer ready to be used(?)

--- oop15-johnny2d/src/it/unibo/oop/utilities/MusicPlayer.java
+++ oop15-johnny2d/src/it/unibo/oop/utilities/MusicPlayer.java
@@ -7,33 +7,50 @@ public interface MusicPlayer {
 
     /**
      * Sets the music on or off.
-     * @param on
-     *          true to set the music on, false to set it off
+     * 
+     * @param musicOn
+     *            true to set the music on, false to set it off
      */
     void setMusic(final boolean musicOn);
-    
-    /**
-     * Plays the audio file.
-     * @param file 
-     *          the audio file to reproduce
-     */
-    void play(final  file);
-    
-    /**
-     * Plays the audio file in loop.
-     * @param file 
-     *          the audio file to reproduce
-     */
-    void playLoop(final  file);
-    
-    /**
-     * Stops the audio file reproduction
-     */
-    void stop();
-    
+
     /**
      * Indicates if the music is on or not.
+     * 
      * @return true if the music is on, false otherwise
      */
     boolean isMusicOn();
+
+    /**
+     * Plays the audio file.
+     * 
+     * @param musicName
+     *            the audio file to reproduce
+     */
+    void play(final String musicName);
+
+    /**
+     * Plays the audio file in loop.
+     * 
+     * @param musicName
+     *            the audio file to reproduce
+     */
+    void playLoop(final String musicName);
+
+    /**
+     * Stops the reproduction of the audio file.
+     * 
+     * @param musicName
+     *            the audio file to stop
+     */
+    void stop(final String musicName);
+
+    /**
+     * Stops the reproduction of all the audio files.
+     */
+    void stopAll();
+
+    /**
+     * Closes the music player.
+     */
+    void closeMusicPlayer();
 }
--- oop15-johnny2d/src/it/unibo/oop/utilities/MusicPlayerImpl.java
+++ oop15-johnny2d/src/it/unibo/oop/utilities/MusicPlayerImpl.java
@@ -1,5 +1,16 @@
 package it.unibo.oop.utilities;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 /**
  * A singleton class that represents a music player used to play the game's
  * music. The music is taken from OpenGameArt.
@@ -9,13 +20,35 @@ package it.unibo.oop.utilities;
 public final class MusicPlayerImpl implements MusicPlayer {
 
     private static final MusicPlayerImpl SINGLETON = new MusicPlayerImpl();
-    private static final String LEVEL_BACKGROUND = "/audio/LevelBackground.mp3";
-    private static final String GAME_OVER = "/audio/GameOver.wav";
+    public static final String LEVEL_BACKGROUND = "LevelBackground";
+    public static final String GAME_OVER = "GameOver";
 
+    private final Map<String, Clip> musicLibrary;
     private boolean musicOn;
 
     private MusicPlayerImpl() {
-        
+        this.musicLibrary = new HashMap<>();
+        this.musicLibrary.put(LEVEL_BACKGROUND, this.loadMusic("/audio/LevelBackground.mp3"));
+        this.musicLibrary.put(GAME_OVER, this.loadMusic("/audio/GameOver.wav"));
+    }
+
+    private Clip loadMusic(final String path) {
+        AudioInputStream ais;
+        Clip clip = null;
+        final URL url = this.getClass().getResource(path);
+        try {
+            ais = AudioSystem.getAudioInputStream(url);
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+            ais.close();
+        } catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        return clip;
+    }
+
+    private Clip getMusic(final String musicName) {
+        return this.musicLibrary.get(musicName);
     }
 
     /**
@@ -33,22 +66,42 @@ public final class MusicPlayerImpl implements MusicPlayer {
     }
 
     @Override
-    public void play() {
-
-    }
-
-    @Override
-    public void playLoop() {
-
-    }
-
-    @Override
-    public void stop() {
-
-    }
-
-    @Override
     public boolean isMusicOn() {
         return this.musicOn;
+    }
+
+    @Override
+    public void play(final String musicName) {
+        if (this.musicOn) {
+            this.getMusic(musicName).setFramePosition(0);
+            this.getMusic(musicName).start();
+        }
+    }
+
+    @Override
+    public void playLoop(final String musicName) {
+        if (this.musicOn) {
+            this.getMusic(musicName).loop(Clip.LOOP_CONTINUOUSLY);
+        }
+    }
+
+    @Override
+    public void stop(final String musicName) {
+        this.getMusic(musicName).stop();
+    }
+
+    @Override
+    public void stopAll() {
+        this.musicLibrary.values().forEach(e -> {
+            e.stop();
+        });
+    }
+
+    @Override
+    public void closeMusicPlayer() {
+        this.musicLibrary.values().forEach(e -> {
+            e.stop();
+            e.close();
+        });
     }
 }
