65cdfeac08964ffb2e25ce7164659811607825a3
Matteo Minardi <7540687+mino181295@users.noreply.github.com>
2016-02-28 14:34:07 +0100

    Modified the relation and improved the code

---
+++ oop15-johnny2d/src/it/unibo/oop/test/TestClass.java
@@ -0,0 +1,20 @@
+package it.unibo.oop.test;
+
+import org.junit.Test;
+
+public class TestClass {
+
+    @Test
+    private void testUtilities() {
+    }
+    private void testCollectables(){
+    	
+    }
+    
+    private void testEnemies(){
+    	
+    }
+    private void testMainCharacter(){
+    	
+    }
+}
