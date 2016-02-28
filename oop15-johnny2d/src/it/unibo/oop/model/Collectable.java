87a635690f21e8962a08d7cd73b1d91aef6a2ff0
Matteo Minardi <7540687+mino181295@users.noreply.github.com>
2016-02-28 17:18:32 +0100

    Gameplay fix on spawn monsters

---
+++ oop15-johnny2d/src/it/unibo/oop/model/Collectable.java
@@ -0,0 +1,15 @@
+package it.unibo.oop.model;
+
+/**
+ * Interface that represents an item placed in the map that can be collected
+ * only by the {@link MainCharacter}.
+ */
+public interface Collectable {
+
+    /**
+     * In case of collision with the {@link MainCharacter} this
+     * {@link Collectable} gets collected.
+     */
+    void collect(MainCharacter bonusCollector);
+
+}
